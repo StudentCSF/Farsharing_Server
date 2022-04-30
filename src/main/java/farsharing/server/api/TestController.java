fde5634f11ee1c3d83358d1280267ec39487166e
Victor Valeev <viktorval2001@gmail.com>
2022-04-30 19:31:03 +0300

    edit test controller

--- src/main/java/farsharing/server/api/TestController.java
+++ src/main/java/farsharing/server/api/TestController.java
@@ -1,34 +1,15 @@
 package farsharing.server.api;
 
-//import farsherver.server.model.entity.AdminEntity;
-
-import farsharing.server.model.entity.UserEntity;
-import farsharing.server.repository.UserRepository;
-//import farsharing.server.repository.UserRoleRepository;
-//import farsharing.server.model.entity.UserRoleEntity;
-//import farsherver.server.model.entity.embeddable.UserIdEmbeddable;
-//import farsherver.server.repository.AdminRepository;
 import io.swagger.v3.oas.annotations.Hidden;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @Hidden
 public class TestController {
 
-    private UserRepository userRepository;
-
-    @Autowired
-    public TestController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @GetMapping("/api")
-    List<String> test() {
-        return new ArrayList<>(List.of("It", "is", "working"));
+    String test() {
+        return "Hello guest!";
     }
 }
