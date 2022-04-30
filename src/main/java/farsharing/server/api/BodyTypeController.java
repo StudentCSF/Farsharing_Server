314f49d1c008a98aea9386b51d49e9f01e7b9eed
Victor Valeev <viktorval2001@gmail.com>
2022-04-30 19:54:58 +0300

    declare other controllers

---
+++ src/main/java/farsharing/server/api/BodyTypeController.java
@@ -0,0 +1,11 @@
+package farsharing.server.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Контроллер типов кузова", description = "Позволяте получать списки типов кузова")
+public class BodyTypeController {
+
+    //TODO
+}
---
+++ src/main/java/farsharing/server/api/LocationController.java
@@ -0,0 +1,11 @@
+package farsharing.server.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "Контроллер локаций", description = "Позволяет получать данных о локациях")
+public class LocationController {
+
+    //TODO
+}
