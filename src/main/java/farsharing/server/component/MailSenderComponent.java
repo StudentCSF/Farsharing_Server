f3a16887c094c8a622dd6253eb0e19e9b55d4161
Victor Valeev <viktorval2001@gmail.com>
2022-09-08 18:45:11 +0300

    impl send mail after pay

--- src/main/java/farsharing/server/component/MailSenderComponent.java
+++ src/main/java/farsharing/server/component/MailSenderComponent.java
@@ -1,5 +1,6 @@
 package farsharing.server.component;
 
+import farsharing.server.model.entity.CarEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -16,6 +17,8 @@ public class MailSenderComponent {
 
     private static final String ACTIVATION_SUBJECT = "Код активации учетной записи в приложении FarSharing";
 
+    private static final String CAR_CODE_SUBJECT = "Код разблокировки автомобиля";
+
     private static final String FROM = "farsharing.sup-port@yandex.com";
 
     @Autowired
@@ -30,10 +33,30 @@ public class MailSenderComponent {
         smm.setFrom(FROM);
         smm.setTo(to);
         smm.setSubject(ACTIVATION_SUBJECT);
-        int code = this.randomizer.nextInt(9000) + 1000;
+        int code = this.generateCode();
         smm.setText(String.format("Ваш код активации: %d\nПожалуйста, введите его в соответствующем поле в приложении для активации учетной записи",
                 code));
         this.mailSender.send(smm);
         return code;
+    }
+
+    public int sendCarCode(CarEntity car, String to) {
+        SimpleMailMessage smm = new SimpleMailMessage();
+        smm.setFrom(FROM);
+        smm.setTo(to);
+        smm.setSubject(CAR_CODE_SUBJECT);
+        Integer code = this.generateCode();
+        smm.setText(String.format(
+                "Вы успешно оформили в прокат %s %s (госномер %s)!\n\nКод разблокировки - %d",
+                car.getBrand(),
+                car.getModel(),
+                car.getStateNumber(),
+                code));
+        this.mailSender.send(smm);
+        return code;
+    }
+
+    private Integer generateCode() {
+        return this.randomizer.nextInt(9000) + 1000;
     }
 }
